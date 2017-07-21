e1615d6e9a68d951b1d64ccfd619ce647d9c3376
Roman Nergachev <nergachev.roman@gmail.com>
2017-07-21 18:26:11 +0300

    Add flag url

--- app/src/main/java/com/rnergachev/contrieslist/binding/CountriesDataBindingComponent.java
+++ app/src/main/java/com/rnergachev/contrieslist/binding/CountriesDataBindingComponent.java
@@ -2,6 +2,8 @@ package com.rnergachev.contrieslist.binding;
 
 import android.databinding.BindingAdapter;
 import android.databinding.DataBindingComponent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -13,8 +15,15 @@ import com.squareup.picasso.Picasso;
  */
 
 public class CountriesDataBindingComponent implements DataBindingComponent {
+    private static String flagUrl = "https://raw.githubusercontent.com/hjnilsson/country-flags/master/png250px/%1$s.png";
+
     @BindingAdapter({"imageUrl"})
-    public static void loadImage(ImageView view, String url) {
-        Picasso.with(view.getContext()).load(url).into(view);
+    public static void loadImage(ImageView view, String id) {
+        if ("BQ".equals(id)) {
+            id = "nl";
+        } else if ("XK".equals(id)) {
+            id = "kosovo";
+        }
+        Picasso.with(view.getContext()).load(String.format(flagUrl, id.toLowerCase())).placeholder(new ColorDrawable(Color.LTGRAY)).into(view);
     }
 }
