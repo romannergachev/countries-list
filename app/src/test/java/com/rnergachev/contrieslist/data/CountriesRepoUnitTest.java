f8e1ce6c70800f20be4cb834af77393b3599be13
Roman Nergachev <nergachev.roman@gmail.com>
2017-07-22 13:54:18 +0300

    Add unit tests

--- app/src/test/java/com/rnergachev/contrieslist/data/CountriesRepoUnitTest.java
+++ app/src/test/java/com/rnergachev/contrieslist/data/CountriesRepoUnitTest.java
@@ -2,17 +2,28 @@ package com.rnergachev.contrieslist.data;
 
 import android.content.Context;
 
+import com.rnergachev.contrieslist.data.model.Country;
 import com.rnergachev.contrieslist.data.network.CountriesApi;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.Single;
 import io.reactivex.android.plugins.RxAndroidPlugins;
+import io.reactivex.observers.TestObserver;
 import io.reactivex.schedulers.Schedulers;
+
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * {@link CountriesRepo} unit test
@@ -27,12 +38,30 @@ public class CountriesRepoUnitTest {
     @Mock
     Context context;
     private CountriesRepo repo;
+    private List<Country> countriesList;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
         repo = new CountriesRepo(api, context);
         RxAndroidPlugins.setInitMainThreadSchedulerHandler(__ -> Schedulers.trampoline());
+        countriesList = new ArrayList<>();
+        countriesList.add(new Country());
+        countriesList.add(new Country());
+    }
+
+    @Test
+    public void repo_should_get_countries() {
+        when(api.getCountries())
+            .thenReturn(Single.just(countriesList));
+
+        TestObserver<List<Country>> testObserver = repo.getCountries().test();
+        verify(api, atLeastOnce()).getCountries();
+        testObserver
+            .awaitTerminalEvent();
+        testObserver
+            .assertNoErrors()
+            .assertValue(list -> list.size() == 2);
     }
 
 
--- app/src/test/java/com/rnergachev/contrieslist/viewmodel/CountriesListViewModelUnitTest.java
+++ app/src/test/java/com/rnergachev/contrieslist/viewmodel/CountriesListViewModelUnitTest.java
@@ -11,6 +11,7 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import io.reactivex.Single;
 
@@ -30,7 +31,7 @@ public class CountriesListViewModelUnitTest {
     @Mock
     private CountriesRepo repo;
     private CountriesListViewModel vm;
-    private ArrayList<Country> data;
+    private List<Country> data;
 
     @Before
     public void setUp() {
@@ -39,5 +40,16 @@ public class CountriesListViewModelUnitTest {
         data = new ArrayList<>();
         data.add(new Country());
         data.add(new Country());
+    }
+
+    @Test
+    public void vm_should_load_countries() {
+        when(repo.getCountries())
+            .thenReturn(Single.just(data));
+
+        vm.loadCountries();
+
+        verify(repo, atLeastOnce()).getCountries();
+        assertEquals(2, vm.countriesList.size());
     }
 }
